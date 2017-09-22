e6e6fa70bfcbd3c61bc65a81b03774651aaa1f2c
Lucas Andrade <lucas@Lucass-MacBook-Pro.local>
2017-09-22 13:43:05 -0400

    Added first files

---
+++ Due/Classes/CustomClasses/NetworkImageView.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkImageView.swift
+//  Due
+//
+//  Created by Lucas Andrade on 9/22/17.
+//  Copyright © 2017 Hera Technologies. All rights reserved.
+//
+
+import Foundation
---
+++ Due/Classes/Views/CellView.swift
@@ -0,0 +1,9 @@
+//
+//  CellView.swift
+//  Due
+//
+//  Created by Lucas Andrade on 9/22/17.
+//  Copyright © 2017 Hera Technologies. All rights reserved.
+//
+
+import Foundation
